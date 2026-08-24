b3aee2f83b0ec563dd8ae256166e932387e5d4d5
JonathanRotta <jrotta19@gmail.com>
2026-08-24 14:38:37 -0300

    cenários de login e senha inválidos

--- cypress/e2e/cyte.cy.js
+++ cypress/e2e/cyte.cy.js
@@ -27,5 +27,43 @@ describe('Testes do site Sauce Demo', ()=>{
 
     });
 
+    it('deve notificar o usuário ao informar senha inválida', () => {
+
+        cy.get('#user-name')
+        .type('standard_user')
+
+        cy.get('#password')
+        .type('senha_invalida', { log: false })
+
+        cy.get('#login-button')
+        .click()
+
+        cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain.text', 'Username and password do not match')
+
+        cy.location('pathname').should('eq', '/')
+
+    });
+
+    it('deve notificar o usuário ao informar login inválido', () => {
+
+        cy.get('#user-name')
+        .type('usuario_invalido')
+
+        cy.get('#password')
+        .type('secret_sauce', { log: false })
+
+        cy.get('#login-button')
+        .click()
+
+        cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain.text', 'Username and password do not match')
+
+        cy.location('pathname').should('eq', '/')
+
+    });
+
 
 })
