e52d77252ce2ad0f4eeb94a243685646e2c6d8f4
Jonathan Rotta <jrotta19@gmail.com>
2026-08-24 20:13:27 -0300

    test: valida item adicionado ao carrinho

--- cypress/e2e/inventory.cy.js
+++ cypress/e2e/inventory.cy.js
@@ -4,15 +4,35 @@ describe('Testes do site Sauce Demo', ()=>{
 
         cy.login()
 
-        
-
+    
     });
 
     
     it('deve apresentar a lista de produtos', () => {
-    cy.get('.inventory_list')
-      .should('be.visible')
-  })
+        cy.get('.inventory_list')
+        .should('be.visible')
+    })
+
+    it('quando adicionar item, deve exibir a quantidade e o item no carrinho', () => {
+        cy.get('#add-to-cart-sauce-labs-backpack').click()
+        cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
+
+        cy.get('[data-test="shopping-cart-link"]').click()
+
+        cy.location('pathname').should('eq','/cart.html')
+
+        cy.get('[data-test="inventory-item"]')
+        .should('have.length', 1)
+        .within(()=>{
+            cy.get('[data-test="inventory-item-name"]')
+            .should('have.text','Sauce Labs Backpack')
+        })
+        
+
+
+    });
+
+
 
 
 
